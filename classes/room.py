ce1eb4781c90ede1f43c967e6e7c3f000b9e44a9
rogertmalg <roger.malgueira@outlook.com>
2021-10-31 18:38:42 +0000

    small changes to simplify functions. q

--- classes/room.py
+++ classes/room.py
@@ -13,7 +13,7 @@ class Room:
     def check_guest_can_pay(self, guest):
         can_pay = True 
         if guest.wallet < self.entry_fee:
-            can_pay = false
+            can_pay = False
         return can_pay
 
     def charge_guest_entry_fee(self, guest):
@@ -21,17 +21,32 @@ class Room:
         guest.wallet -= self.entry_fee
     
     def check_in_guest(self, guest):
-        if self.capacity > 0 and self.check_guest_can_pay(guest) == True:
+        if self.check_guest_can_pay(guest) == False:
+            return "You don't have enough money"
+        
+        elif self.capacity > 0:
             self.charge_guest_entry_fee(guest)
             self.add_guest_to_room(guest)
             self.capacity -= 1
-
+ 
         return "Sorry, this room is full"
 
     def check_out_guest_by_name(self, guest_name):
-        check_out = [self.guests_in_room.remove(guest) for guest in self.guests_in_room if guest.name == guest_name]
-        return check_out
+        [self.guests_in_room.remove(guest) for guest in self.guests_in_room 
+        if guest.name == guest_name]
+        
         
     def add_song_to_play_list(self, song):
-        self.play_list.append(song)      
+        self.play_list.append(song)
 
+    def remove_song_by_name(self, song_name):
+        [self.play_list.remove(song) for song in self.play_list 
+        if song.name == song_name]      
+        
+
+    # def remove_song_by_singer(self, singer_name):
+    #     [self.play_list.remove(song) for song in self.play_list 
+    #     if song.singer == singer_name]
+
+        
+    
--- tests/room_test.py
+++ tests/room_test.py
@@ -1,4 +1,5 @@
 import unittest
+import pdb
 from classes.room import Room
 from classes.song import Song
 from classes.guest import Guest
@@ -8,8 +9,10 @@ class TestRoom(unittest.TestCase):
         self.room1 = Room("CCC_01", 5, 4.00)
         self.guest1 = Guest("Linda", 30, 50.00)
         self.guest2 = Guest("Bob", 39, 70.00)
+        self.guest3 = Guest("Tina", 13, 3.00)
         self.song1 = Song("Wannabe", "Spice Girls")
         self.song2 = Song("I kissed a girl", "Katy Perry")
+        self.song3 = Song("Last Friday night", "Katy Perry")
         
     
     def test_room_has_name(self):
@@ -23,7 +26,8 @@ class TestRoom(unittest.TestCase):
 
     def test_add_guest_to_room(self):
         self.room1.add_guest_to_room(self.guest1)
-        self.assertEqual(1, len(self.room1.guests_in_room))
+        self.room1.add_guest_to_room(self.guest2)
+        self.assertEqual(2, len(self.room1.guests_in_room))
     
     def test_guest_can_pay(self):
         self.assertEqual(True, self.room1.check_guest_can_pay(self.guest1))
@@ -44,10 +48,14 @@ class TestRoom(unittest.TestCase):
         self.room1.capacity = 0
         self.assertEqual("Sorry, this room is full", self.room1.check_in_guest(self.guest2))
 
+    def test_check_in_guest_cant_pay(self):
+        self.room1.check_in_guest(self.guest3)
+        self.assertEqual(5, self.room1.capacity)
+        
     def test_check_out_guest(self):
-        self.room1.guests_in_room = [self.guest1, self.guest2]
+        self.room1.guests_in_room = [self.guest1, self.guest2, self.guest3]
         self.room1.check_out_guest_by_name("Linda")
-        self.assertEqual(1, len(self.room1.guests_in_room))
+        self.assertEqual(2, len(self.room1.guests_in_room))
 
     def test_check_out_guest_not_in_room(self):
         self.room1.guests_in_room = [self.guest1, self.guest2]
@@ -58,4 +66,16 @@ class TestRoom(unittest.TestCase):
         self.room1.add_song_to_play_list(self.song1)
         self.room1.add_song_to_play_list(self.song2)
         self.assertEqual(2, len(self.room1.play_list))
+    
+    def test_remove_song_by_name(self):
+        self.room1.play_list = [self.song1, self.song2, self.song3]
+        self.room1.remove_song_by_name("Wannabe")
+        self.assertEqual(2, len(self.room1.play_list))
+        
+    # def test_remove_song_by_singer_name(self):
+    #     self.room1.play_list = [self.song1, self.song2, self.song3]
+    #     self.room1.remove_song_by_singer("Katy Perry")
+    #     self.assertEqual(1, len(self.room1.play_list))
+
+    # cant get this to work properly 
     
